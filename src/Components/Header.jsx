f25b543c520db018ec0f26c4ebde5676a4819bb8
Travis Lee <tlee91@uic.edu>
2023-04-24 23:26:53 -0500

    moved black bar to header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography'
+
+class Header extends Component {
+    render() {
+    	return(
+      	<Box sx={{width: 5000, height: 50, backgroundColor: 'black'}}></Box>
+      );
+      }
+    }
+
+export default Header;
--- src/Pages/Manualadjustments.jsx
+++ src/Pages/Manualadjustments.jsx
@@ -7,21 +7,12 @@ import Typography from '@mui/material/Typography'
 class ManualAdjust extends Component {
     render() {
     	return(
-      	<Box sx={{width: 5000, height: 50, backgroundColor: 'black'}}>
-				{/* Stack for the text titles for "Inputs" and "Preview" Towards the top */}
-					<Stack 
-						direction = "row" 
-						justify = "Center" 
-						alignItems = "center" 
-						spacing = {24}>
+				<Stack direction = "row" justify = "Center" alignItems = "center" spacing = {24} >
 
 							<Typography color="white" variant="h4">Inputs</Typography>
 							<Typography color="white" variant="h4">Preview</Typography>
 
 					</Stack>
-					<Stack direction = "vertical"><Typography variant = "h1">test</Typography></Stack>
-				</Box>
-		
   );
 }
 }
