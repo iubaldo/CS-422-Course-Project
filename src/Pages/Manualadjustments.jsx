2f1610acb7d385499390e7cea42e45994c7c4ab5
Travis Lee <tlee91@uic.edu>
2023-04-24 22:19:53 -0500

    updated import

--- src/Pages/Manualadjustments.jsx
+++ src/Pages/Manualadjustments.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import Stack from '@mui/joy/Stack';
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 
 class ManualAdjust extends Component {
     render() {
-     return(
-            <Box
-                sx={{
-                    width: 300, height: 300, backgroundColor: 'primary.dark',
-                    '&:hover': { backgroundColor: 'primary.main', 
-                    opacity: [0.9, 0.8, 0.7],
-                    },
-                }}
-            />
-        );
-    }
+    	return(
+      	<Box
+          sx={{
+            width: 300, height: 300, backgroundColor: 'primary.dark',
+            '&:hover': { backgroundColor: 'primary.main', 
+            opacity: [0.9, 0.8, 0.7],
+            },
+        }}
+    />
+  );
+}
 }
