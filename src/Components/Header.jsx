4ca2c3352049235699fb884f4da3232cf145698b
Travis Lee <tlee91@uic.edu>
2023-04-25 01:44:22 -0500

    Updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack';
 
 class Header extends Component {
     render() {
     	return(
-      	<Box sx={{width: 1920, height: 80, backgroundColor: 'Grey'}}></Box>
+      	<Box sx={{width: "100%", height: "100%", backgroundColor: 'Grey'}}>
+          <Stack direction="row" spacing={50}>
+            <img style={{width:"10%", height:"10%"}} src="/Images/logo.png" alt="logo"/>
+
+
+          </Stack>
+
+
+        </Box>
       );
       }
     }
